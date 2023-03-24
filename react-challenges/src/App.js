34d34e96b91c0bc94fb79abc7b487284014f01e8
Cheyenne Vang <cheyennevang@Administrators-MacBook-Pro-7.local>
2023-03-24 15:19:11 -0400

    Challenges

--- react-challenges/src/App.js
+++ react-challenges/src/App.js
@@ -8,16 +8,22 @@ class App extends React.Component {
 // You need a constructor in your class component where you want the data to be updated/your local state 
   constructor(props) {
       super(props);
+      // this.state = {
+      //   person: {}
+      // };
+
       // Hard - Create an array and iterate through it in return()
       this.state = {
           people: [
           {
+            id: 1,
             name: 'Cheyenne',
             number: '10',
             dateOfBirth: 'Sept'
           },
           {
-            name: 'Lyndsey',
+            id: 2,
+            name: 'Astrid',
             number: '11',
             dateOfBirth: 'Dec'
           }
@@ -26,15 +32,29 @@ class App extends React.Component {
   }
   
   render() {
+    // Very Easy
+    // return(
+    //   <div>
+    //     <p>Cheyenne</p>
+    //     <p>10</p>
+    //     <p>Sept</p>
+    //   </div>
+    // );
+
+    // Two different ways to return();
+    // return(
+    //   // passing state into the BasicInfo component
+    //   this.state.people.map((person) => {
+    //     return (
+    //       <BasicInfo
+    //       p={person} />
+    //       // p is our prop, which is our objects in the people array
+    //     );
+    //   })
+    // )
     return(
-      this.state.people.map((person) => {
-        return (
-          <BasicInfo
-          p={person} />
-          // p is our prop, which is our objects in the people array
-        );
-      })
-    )
+      this.state.people.map(person => <BasicInfo key={person.id} p={person} />)
+    );
   }
 }
 
--- react-challenges/src/components/medium/BasicInfo.js
+++ react-challenges/src/components/medium/BasicInfo.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import './BasicInfo.css';
 
 // Medium
-// Passing props to this component that contains the structure for the information to display on the browser.
 class BasicInfo extends Component {
     render() {
         return (
-            <div style={{marginBottom: '40px'}}>
-              <p>{this.props.p.name}</p>
-              <p>{this.props.p.number}</p>
-              <p>{this.props.p.dateOfBirth}</p>
+            // this.props is the object that contains the attributes that we passed in the parent component, which is our App.js
+            <div>
+                <h2>{`Person: ${this.props.p.id}`}</h2>
+                <p>{this.props.p.name}</p>
+                <p>{this.props.p.number}</p>
+                <p>{this.props.p.dateOfBirth}</p>
             </div>
         );
     }
 }
 
 export default BasicInfo;
+
+// How to pass state into a class component?
+// You can pass state as a prop
